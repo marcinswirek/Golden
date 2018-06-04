4bc20d0acc6f1b7e1dc4766c76e9353c54bc4736
Marcin Świrek <swirek.marcin@gmail.com>
2018-06-04 13:57:16 +0200

    Add fixed position on navigation. Add onscroll background color on navigation

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const navMenu = document.getElementById("nav_bar");
+
+function menuScroll() {
+  console.log(document.body.scrollTop);
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    navMenu.className = "on_scroll_color";
+  } else {
+    navMenu.className = "navigation";
+  }
+}
+
+window.onscroll = function() {
+  menuScroll();
+};
