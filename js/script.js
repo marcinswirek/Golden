3088905ab0337c4974d8d5ca032ffd906befdd97
Marcin Świrek <swirek.marcin@gmail.com>
2018-06-05 21:35:32 +0200

    Fix onscroll down menu height

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 const navMenu = document.getElementById("nav_bar");
 
 function menuScroll() {
-  //   console.log(document.body.scrollTop);
   if (
     document.body.scrollTop > 100 ||
     document.documentElement.scrollTop > 100
