597f244fdb650f0ebaf4f57e8860ae3394d39bba
Marcin Świrek <swirek.marcin@gmail.com>
2018-07-28 10:38:33 +0200

    Add mlab db

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ var express = require("express");
 var enforce = require("express-sslify");
 var app = express();
 var port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://marcin:marcin1@ds257851.mlab.com:57851/test-app");
 
 // Use enforce.HTTPS({ trustProtoHeader: true }) in case you are behind
 // a load balancer (e.g. Heroku). See further comments below
